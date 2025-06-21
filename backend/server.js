a9d27c77b83dc60ce618b8072b2073529c1923fd
Hemachandran53 <110321473+Hemachandran53@users.noreply.github.com>
2025-06-22 00:16:41 +0530

    1st part completed - backend

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+const Port = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/message', messageRoutes);
+app.use('/api/users', userRoutes);
+
+app.listen(Port, () => {
+    connectToMongoDB();
+    console.log(`Server running on port ${Port}`);
+});
